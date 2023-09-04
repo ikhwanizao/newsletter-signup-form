260608fc1ff2d3b32fa4c240e5ca7f033227adf7
ikhwanizao <ikhwanm32@gmail.com>
2023-09-04 16:57:37 +0800

    Complete confirmation

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const submitButton = document.querySelector("#email-submit");
+const dismissButton = document.querySelector(".dismiss-msg");
 const emailInput = document.querySelector('input[type="email"]');
 const invalidEmailElements = document.getElementsByClassName("invalid-email");
+const centerContainer = document.querySelector(".center-container");
+const confirmationContainer = document.querySelector(".confirmation-container");
 
-submitButton.addEventListener("click", () => {
+function emailValidation() {
   const emailAddress = emailInput.value;
   const validRegex =
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
@@ -15,5 +18,25 @@ submitButton.addEventListener("click", () => {
 
   // Set borderColor and backgroundColor based on isValidEmail
   emailInput.style.borderColor = isValidEmail ? "#ccc" : "var(--Tomato)";
-  emailInput.style.backgroundColor = isValidEmail ? "var(--White)" : "var(--Tomato-bg)";
+  emailInput.style.backgroundColor = isValidEmail
+    ? "var(--White)"
+    : "var(--Tomato-bg)";
+
+  if (isValidEmail) {
+    centerContainer.style.display = "none";
+    confirmationContainer.style.display = "initial";
+  }
+}
+
+function dismissConfirmation() {
+  centerContainer.style.display = "flex";
+  confirmationContainer.style.display = "none";
+}
+
+submitButton.addEventListener("click", () => {
+  emailValidation();
+});
+
+dismissButton.addEventListener("click", () => {
+  dismissConfirmation();
 });
